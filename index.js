4133ea6b673c1f164bde36f14ebed031d521cf78
Adam Cholmondeley <sovchickendipps@gmail.com>
2024-02-06 09:40:51 +0000

    Changed to not use appendChild

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function showList(elem, url) {
     data.forEach((item) => {
         const li = document.createElement('li');
         li.textContent = item;
-        elem.appendChild(li);
+        elem.append(li);
     });
 }
 
